9a5e7df4e1006aacbbe6a7811b1c36caac43d6cc
Laurent Almeras <laurent.almeras@kobalt.fr>
2022-11-14 01:16:27 +0100

    wip

--- pkg/cmd/cloud.go
+++ pkg/cmd/cloud.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"net/url"
 
 	"github.com/lalmeras/clio/pkg/types"
 	"github.com/lalmeras/clio/pkg/util"
@@ -21,31 +22,111 @@ var projectCmd = &cobra.Command{
 	Short: "Public Cloud API",
 }
 
+var imageCmd = &cobra.Command{
+	Use:   "image",
+	Short: "Public Cloud API",
+}
+
+type ImageFilter struct {
+	FlavorType string
+	OSType     string
+	Region     string
+}
+
+var ProjectID string
+var imageFilter = ImageFilter{}
+
 func init() {
+	cloudCmd.AddCommand(projectsCmd)
 	cloudCmd.AddCommand(projectCmd)
-	projectCmd.AddCommand(projectsCmd)
 	projectCmd.AddCommand(projectGetCmd)
+	projectCmd.AddCommand(imageCmd)
+	imageCmd.AddCommand(imageListCmd)
+	imageCmd.AddCommand(imageGetCmd)
+
+	projectCmd.PersistentFlags().StringVarP(&ProjectID, "project", "p", "", "Project ID")
+
+	imageListCmd.Flags().StringVarP(&imageFilter.FlavorType, "flavor-type", "f", "", "Flavor type filter")
+	imageListCmd.Flags().StringVarP(&imageFilter.OSType, "os-type", "o", "", "OS type filter")
+	imageListCmd.Flags().StringVarP(&imageFilter.Region, "region", "r", "", "Region filter")
+
+	projectCmd.MarkPersistentFlagRequired("project")
 }
 
 var projectsCmd = &cobra.Command{
-	Use:   "list",
+	Use:   "projects",
 	Short: "Project management",
 	Run: func(cmd *cobra.Command, args []string) {
 		projects := []string{}
 		util.Check(util.OvhGet("/cloud/project", &projects))
-		fmt.Printf("%+v\n", projects)
+		for _, projectID := range projects {
+			project, err := Project(projectID)
+			util.Check(err)
+			fmt.Printf("%+v\n", *project)
+		}
 	},
 }
 
 var projectGetCmd = &cobra.Command{
 	Use:   "get",
 	Short: "Project detail",
+	Args:  cobra.ExactArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		project, err := Project(ProjectID)
+		util.Check(err)
+		fmt.Printf("%+v\n", *project)
+	},
+}
+
+var imageListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "Project management",
+	Args:  cobra.ExactArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		values := url.Values{}
+		if imageFilter.FlavorType != "" {
+			values.Set("flavorType", imageFilter.FlavorType)
+		}
+		if imageFilter.OSType != "" {
+			values.Set("osType", imageFilter.OSType)
+		}
+		if imageFilter.Region != "" {
+			values.Set("region", imageFilter.Region)
+		}
+		murl := "/cloud/project/" + ProjectID + "/image?" + values.Encode()
+		images := []types.Image{}
+		util.Check(util.OvhGet(murl, &images))
+		for _, image := range images {
+			fmt.Printf("%+v\n", image)
+		}
+	},
+}
+
+var imageGetCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Project detail",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		projectID := args[0]
-		url := "/cloud/project/" + projectID
-		project := types.Project{}
-		util.Check(util.OvhGet(url, &project))
-		fmt.Printf("%+v\n", project)
+		image, err := Image(ProjectID, args[0])
+		util.Check(err)
+		fmt.Printf("%+v\n", *image)
 	},
+}
+
+func Project(projectID string) (*types.Project, error) {
+	url := "/cloud/project/" + projectID
+	project := &types.Project{}
+	if err := util.OvhGet(url, &project); err != nil {
+		return nil, err
+	}
+	return project, nil
+}
+
+func Image(projectID string, imageID string) (*types.Image, error) {
+	murl := "/cloud/project/" + projectID + "/image/" + imageID
+	image := &types.Image{}
+	if err := util.OvhGet(murl, &image); err != nil {
+		return nil, err
+	}
+	return image, nil
 }
--- pkg/types/cloud.go
+++ pkg/types/cloud.go
@@ -15,3 +15,20 @@ type Project struct {
 	Status       string    `json:"status"`
 	Unleash      bool      `json:"unleash"`
 }
+
+type Image struct {
+	CreationDate time.Time `json:"creationDate"`
+	FlavorType   string    `json:"flavorType"`
+	ID           string    `json:"id"`
+	MinDisk      int64     `json:"minDisk"`
+	MinRam       int64     `json:"minRam"`
+	Name         string    `json:"name"`
+	PlanCode     string    `json:"planCode"`
+	Region       string    `json:"region"`
+	Size         float32   `json:"size"`
+	Status       string    `json:"status"`
+	Tags         []string  `json:"tags"`
+	Type         string    `json:"type"`
+	User         string    `json:"user"`
+	Visibility   string    `json:"visibility"`
+}
